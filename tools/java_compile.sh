c3f559e9d6ef3d17f10871696e1c0514765be4cf
huangdongdonnie <huangdongdonnie@didichuxing.com>
2019-10-17 14:24:39 +0800

    fix some bugs

--- tools/java_compile.sh
+++ tools/java_compile.sh
@@ -1,35 +1,104 @@
 #!/bin/sh
 
+function check_thrift_env() {
+    local thrift_version=$1
+    local tre_path=$(dirname $0)/../env/$thrift_version
+    if [ ! -d "$tre_path" ]; then
+        echo "Unsupported Thrift version: $thrift_version"
+        exit 1
+    fi
+    local tre_path=$(cd $tre_path; pwd)
+    if [ $(find $tre_path -name "*.jar" | wc -l) == 0 ]; then
+        echo "Unsupported Thrift version: $thrift_version"
+        exit 1
+    fi
+}
+
+function check_java_src_files() {
+    local java_src_dir=$1
+    if [ ! -d "$java_src_dir" ]; then
+        echo "Invalid java src dir: \"$java_src_dir\""
+        exit 1
+    fi
+    local java_src_cnt=$(find $java_src_dir -name "*.java" | wc -l | awk '{print $1}')
+    if [ $java_src_cnt == 0 ]; then
+        echo "Empty java src dir: \"$java_src_dir\", files(*.java) not found"
+        exit 1
+    fi
+}
+
+function compile_java_src_files() {
+    local tre_path=$1
+    local java_src_dir=$2
+    local out_dir=$3
+
+    echo "Thrift Runtime Environment: \"$tre_path\""
+    echo "Java source: \"$java_src_dir\", files(*.java) found: $java_src_cnt"
+    echo "Output dir: \"$out_dir\""
+
+    # java tmp dir, copy src to tmp dir
+    local java_tmp_dir=$out_dir/tmp_src
+    rm -rf $java_tmp_dir
+    mkdir $java_tmp_dir
+    # find $java_src_dir -name "*.java" | xargs cp -t $java_tmp_dir
+    # if [[ "$?" != "0" ]]; then
+    #     rm -rf $java_tmp_dir
+    #     exit 1
+    # fi
+    for file in $(find $java_src_dir -name "*.java"); do
+        cp $file $java_tmp_dir
+    done
+
+    # compile to *.class
+    local classes_dir=$out_dir/classes
+    rm -rf $classes_dir
+    mkdir $classes_dir
+    javac -cp :$tre_path/* -d $classes_dir $java_tmp_dir/*.java
+    if [[ "$?" != "0" ]]; then
+        rm -rf $classes_dir
+        rm -rf $java_tmp_dir
+        exit 1
+    else
+        echo "compile java files success, classes dir: \"$classes_dir\""
+        rm -rf $java_tmp_dir
+    fi
+
+    # No need to compile as a jar
+    compress_as_jar $classes_dir $out_dir/classes.jar
+}
+
+function compress_as_jar() {
+    local classes_dir=$1
+    local jar_path=$2
+    rm -rf $jar_path
+    cd $classes_dir
+    jar -cvf $jar_path ./*
+    if [[ "$?" != "0" ]]; then
+        exit 1
+    fi
+    echo "compress as jar file: \"$jar_path\""
+}
+
+javac -version
+if [[ "$?" != "0" ]]; then
+    exit 1
+fi
+
 if [ $# -lt 3 ]; then
     echo "too less arguments"
     exit 1
 fi
 
 # Thrift version
-thrift_version=$1 
-tre_path=$(dirname $0)/../env/$thrift_version
-if [ ! -d "$tre_path" ]; then
-    echo "Unsupported Thrift version: $thrift_version"
-    exit 1
-fi
-tre_path=$(cd $tre_path; pwd)
-if [ $(find $tre_path -name "*.jar" | wc -l) == 0 ]; then
-    echo "Unsupported Thrift version: $thrift_version"
-    exit 1
-fi
+check_thrift_env $1
+tre_path=$(cd $(dirname $0)/../env/$1; pwd)
 
 # java src dir
-java_src_dir=$2
-if [ ! -d "$java_src_dir" ]; then
-    echo "Invalid java src dir: \"$java_src_dir\""
+check_java_src_files $2
+if [[ "$?" != "0" ]]; then
     exit 1
 fi
-java_src_dir=$(cd $java_src_dir; pwd)
-java_src_cnt=$(find $java_src_dir -name "*.java" | wc -l | awk '{print $1}')
-if [ $java_src_cnt == 0 ]; then
-    echo "Empty java src dir: \"$java_src_dir\", files(*.java) not found"
-    exit 1
-fi
+java_src_dir=$(cd $2; pwd)
 
 # output dir
 out_dir=$3
@@ -38,26 +107,4 @@ if [ ! -d "$out_dir" ]; then
 fi
 out_dir=$(cd $out_dir; pwd)
 
-# java tmp dir, copy src to tmp dir
-java_tmp_dir=$out_dir/tmp_src
-rm -rf $java_tmp_dir
-mkdir $java_tmp_dir
-find $java_src_dir -name "*.java" | xargs cp -t $java_tmp_dir
-
-java -version
-
-echo "Thrift Runtime Environment: \"$tre_path\""
-echo "Java source: \"$java_src_dir\", files(*.java) found: $java_src_cnt"
-echo "Output dir: \"$out_dir\""
-
-classes_dir=$out_dir/classes
-rm -rf $classes_dir
-mkdir $classes_dir
-javac -cp :$tre_path/* -d $classes_dir $java_tmp_dir/*.java
-rm -rf $java_tmp_dir
-
-## No need to compile as a jar
-# jar_path=$out_dir/classes.jar
-# rm -rf $jar_path
-# cd $classes_dir
-# jar -cvf $jar_path ./*
+compile_java_src_files $tre_path $java_src_dir $out_dir
--- tools/thrift_compile.sh
+++ tools/thrift_compile.sh
@@ -1,33 +1,67 @@
 #!/bin/sh
 
-if [ $# -lt 2 ]; then
+function gen_thrift_file() {
+    local out_dir=$2
+    local src_file=$1
+    echo "IDL source: \"$src_file\""
+    thrift -r -gen java -o $out_dir $src_file
+    if [[ "$?" != "0" ]]; then
+        exit 1
+    fi
+    echo "gen java from thrift source file success!"
+}
+
+function gen_thrift_files() {
+    local out_dir=$2
+    local src_dir=$(cd $1; pwd)
+    local src_cnt=$(find $src_dir -name "*.thrift" | wc -l | awk '{print $1}')
+    if [[ $src_cnt == 0 ]]; then
+        echo "Empty IDL src dir: \"$src_dir\", files(*.thrift) not found"
+        exit 1
+    fi
+    echo "IDL source: \"$src_dir\", files(*.thrift) found: $src_cnt"
+    # find $src_dir -name "*.thrift" | xargs -i thrift -r -gen java -o $out_dir {}
+    # if [[ "$?" != "0" ]]; then
+    #     exit 1
+    # fi
+    for file in $(find $src_dir -name "*.thrift"); do
+    	thrift -r -gen java -o $out_dir $file
+    done
+
+    echo "gen java from thrift source files success!"
+}
+
+function compile_java_files() {
+    local out_dir=$1
+    local thrift_version=$(thrift -version | awk '{print $3}')
+    sh $(cd "$(dirname $0)"; pwd)/java_compile.sh $thrift_version $out_dir/gen-java $out_dir
+}
+
+thrift -version
+if [[ "$?" != "0" ]]; then
+    exit 1
+fi
+
+if [[ $# -lt 2 ]]; then
     echo "too less arguments"
     exit 1
 fi
 
-# IDL src dir
-src_dir=$1
-if [ ! -d "$src_dir" ]; then
-    echo "Invalid IDL src dir: \"$src_dir\""
-    exit 1
-fi
-src_dir=$(cd $1; pwd)
-src_cnt=$(find $src_dir -name "*.thrift" | wc -l | awk '{print $1}')
-if [ $src_cnt == 0 ]; then
-    echo "Empty IDL src dir: \"$src_dir\", files(*.thrift) not found"
-    exit 1
-fi
-
 # output dir
-out_dir=$2
-if [ ! -d "$out_dir" ]; then
-    mkdir -p $out_dir
+if [[ ! -d "$2" ]]; then
+    mkdir -p $2
 fi
-out_dir=$(cd $out_dir; pwd)
+out_dir=$(cd $2; pwd)
 
-thrift -version
-echo "IDL source: \"$src_dir\", files(*.thrift) found: $src_cnt"
-find $src_dir -name "*.thrift" | xargs -i thrift -r -gen java -o $out_dir {}
+# IDL source
+if [[ -f "$1" ]]; then
+    gen_thrift_file $1 $out_dir
+elif [[ -d "$1" ]]; then
+    src_dir=$(cd $1; pwd)
+    gen_thrift_files $1 $out_dir
+else
+    echo "Invalid IDL source: \"$1\""
+    exit 1
+fi
 
-thrift_version=$(thrift -version | awk '{print $3}')
-sh $(cd "$(dirname $0)"; pwd)/java_compile.sh $thrift_version $out_dir/gen-java $out_dir
+compile_java_files $out_dir
